c386c78a56992832c7c575ca322320e3b5fbca53
nullBlade <stepan.gaidukevich@gmail.com>
2023-03-12 07:54:10 +0200

    changes

--- src/main/java/org/waveapi/api/WaveMod.java
+++ src/main/java/org/waveapi/api/WaveMod.java
@@ -4,14 +4,8 @@ import java.io.File;
 
 public abstract class WaveMod {
 
-    public final String name;
-    public final String version;
-
     public WaveMod(String name, String version) {
-        this.name = name;
-        this.version = version;
 
-        WaveLoader.register(this);
     }
 
 
