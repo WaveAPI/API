dd6fd2ae7c25b834a70db1678bb8af4a3c95d33f
nullBlade <stepan.gaidukevich@gmail.com>
2023-03-12 14:10:34 +0200

    recipes

---
+++ src/main/java/org/waveapi/api/content/items/WaveShapedRecipe.java
@@ -0,0 +1,12 @@
+package org.waveapi.api.content.items;
+
+import org.waveapi.api.WaveMod;
+
+public class WaveShapedRecipe {
+
+    public WaveShapedRecipe(String result, String[] pattern, WaveMod mod) {}
+    public WaveShapedRecipe(WaveItem result, String[] pattern, WaveMod mod) {}
+
+    public WaveShapedRecipe addIngredient(char symbol, String id) {return this;}
+
+}
