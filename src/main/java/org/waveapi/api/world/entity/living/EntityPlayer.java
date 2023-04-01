e98dcab61856ca70867b6cdfb5015e2d66e3ebbf
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-01 11:18:32 +0300

    give item

--- src/main/java/org/waveapi/api/world/entity/living/EntityPlayer.java
+++ src/main/java/org/waveapi/api/world/entity/living/EntityPlayer.java
@@ -1,14 +1,15 @@
 package org.waveapi.api.world.entity.living;
 
-import org.waveapi.api.content.entities.EntityCreation;
 import org.waveapi.api.misc.Text;
+import org.waveapi.api.world.inventory.ItemStack;
 
-public class EntityPlayer extends EntityLiving {
-    public EntityPlayer(EntityCreation e) {super(e);}
-
+public abstract class EntityPlayer extends EntityLiving {
+    public EntityPlayer() {super(null);}
     public void sendMessage(String message) {}
 
     public void sendMessage(Text message) {}
+
+    public void giveItem(ItemStack item) {}
     public void sendActionBar(String message) {}
     public void sendActionBar(Text message) {}
 
