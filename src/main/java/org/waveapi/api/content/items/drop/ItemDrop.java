8474bcc6442006ef507b4c1a8375f65a0705440a
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-02 07:35:39 +0300

    Forgot extends

--- src/main/java/org/waveapi/api/content/items/drop/ItemDrop.java
+++ src/main/java/org/waveapi/api/content/items/drop/ItemDrop.java
@@ -2,7 +2,7 @@ package org.waveapi.api.content.items.drop;
 
 import org.waveapi.api.content.items.WaveItem;
 
-public class ItemDrop {
+public class ItemDrop extends Drop {
 
     public ItemDrop(String item, int min, int max) {}
     public ItemDrop(WaveItem item, int min, int max) {}
