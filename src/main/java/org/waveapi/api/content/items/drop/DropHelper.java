19019f6acefa57b259c5de1756fda4b8b77e8288
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-02 09:33:08 +0300

    ore in DropHelper

--- src/main/java/org/waveapi/api/content/items/drop/DropHelper.java
+++ src/main/java/org/waveapi/api/content/items/drop/DropHelper.java
@@ -1,4 +1,17 @@
 package org.waveapi.api.content.items.drop;
 
+import org.waveapi.api.content.items.WaveItem;
+import org.waveapi.api.content.items.block.WaveBlock;
+
+import java.util.Objects;
+
 public class DropHelper {
+    public static Drop ore(WaveBlock block, WaveItem item) {
+        return Objects.requireNonNull(null);
+    }
+
+    public static Drop ore(WaveBlock block, WaveItem item, int min, int max) {
+        return Objects.requireNonNull(null);
+    }
+
 }
