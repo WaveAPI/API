c274912c99595cb4eef49fa9adbe8fe9e5af9b51
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-07 07:32:31 +0300

    Missed a few parameters

--- src/main/java/org/waveapi/api/content/items/armour/WaveArmourItem.java
+++ src/main/java/org/waveapi/api/content/items/armour/WaveArmourItem.java
@@ -4,7 +4,7 @@ import org.waveapi.api.WaveMod;
 import org.waveapi.api.content.items.WaveItem;
 
 public class WaveArmourItem extends WaveItem {
-    public WaveArmourItem(String id, WaveMod mod, ArmourSlot slot) {
+    public WaveArmourItem(String id, WaveArmourMaterial material, ArmourSlot slot, WaveMod mod) {
         super(id, mod);
     }
 }
