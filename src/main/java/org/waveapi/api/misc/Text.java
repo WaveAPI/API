c65e64ba58e578e5e49e8fb37fc9c10341d8cc09
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-08 08:22:04 +0300

    Text.java

--- src/main/java/org/waveapi/api/misc/Text.java
+++ src/main/java/org/waveapi/api/misc/Text.java
@@ -5,6 +5,10 @@ import java.util.Objects;
 public class Text {
     public String getText() {return "";}
     public static Text plain(String text) {return Objects.requireNonNull(null);}
-    public static Text translatable(String text) {return Objects.requireNonNull(null);}
+
+    /**
+     * For if you know translation keys
+     */
+    public static Text translatable(String id) {return Objects.requireNonNull(null);}
 
 }
