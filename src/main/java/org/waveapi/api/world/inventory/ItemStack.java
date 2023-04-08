4223d3172495da91cc08b27a5cc2c716f78ca315
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-08 14:28:48 +0300

    item damage

--- src/main/java/org/waveapi/api/world/inventory/ItemStack.java
+++ src/main/java/org/waveapi/api/world/inventory/ItemStack.java
@@ -1,9 +1,13 @@
 package org.waveapi.api.world.inventory;
 
+import org.waveapi.api.world.entity.living.EntityPlayer;
+
 public class ItemStack {
 
     public String getName() {return java.util.Objects.requireNonNull(null);}
     public int getAmount() {return 0;}
     public void setAmount(int amount) {}
+
+    public void damage(int amount, EntityPlayer player) {}
 
 }
