078c383a2d2fedd5627bc172bb255542fb06e8f5
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-08 08:21:22 +0300

    TranslatedText.java

--- src/main/java/org/waveapi/api/misc/TranslatedText.java
+++ src/main/java/org/waveapi/api/misc/TranslatedText.java
@@ -1,9 +1,16 @@
 package org.waveapi.api.misc;
 
-public class TranslatedText {
+import org.waveapi.api.WaveMod;
 
-    public TranslatedText() {}
+public class TranslatedText extends Text {
+
+    public TranslatedText(String name, WaveMod mod) {}
 
     public TranslatedText addTranslation(String language, String translation) {return this;}
+
+    /**
+     * Replaces % in translation with objects
+     */
+    public Text withValues(Object... objects) {return this;}
 
 }
