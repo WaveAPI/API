40d3dacd7a05382be37f5c5dcfd8768886bf8621
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-08 19:46:57 +0300

    start

---
+++ src/main/java/org/waveapi/api/content/items/block/blockentities/types/ContainerTile.java
@@ -0,0 +1,18 @@
+package org.waveapi.api.content.items.block.blockentities.types;
+
+import java.util.Objects;
+
+public interface ContainerTile {
+
+    int getSize();
+    org.waveapi.api.world.inventory.ItemStack getStack(int slot);
+    void setStack(int slot, org.waveapi.api.world.inventory.ItemStack stack);
+
+    default org.waveapi.api.world.inventory.ItemStack take(int slot, int amount) {
+        return Objects.requireNonNull(null);
+    }
+
+    default org.waveapi.api.world.inventory.ItemStack take(int slot) {
+        return Objects.requireNonNull(null);
+    }
+}
