1491ff8f723a9368bac1986d797cfa340e49a71c
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-08 08:39:12 +0300

    I should have probably did this on another branch

--- src/main/java/org/waveapi/api/content/items/block/WaveBlock.java
+++ src/main/java/org/waveapi/api/content/items/block/WaveBlock.java
@@ -7,15 +7,12 @@ import org.waveapi.api.content.items.block.model.BlockModel;
 import org.waveapi.api.content.items.drop.Drop;
 import org.waveapi.api.content.items.drop.ItemDrop;
 import org.waveapi.api.math.BlockPos;
-import org.waveapi.api.misc.Text;
 import org.waveapi.api.world.entity.living.EntityPlayer;
-import org.waveapi.api.world.inventory.ItemStack;
 import org.waveapi.api.world.inventory.ItemUseResult;
 import org.waveapi.api.world.inventory.UseHand;
 import org.waveapi.api.world.world.BlockState;
 import org.waveapi.api.world.world.World;
 
-import java.util.List;
 import java.util.Objects;
 
 public class WaveBlock extends WaveItem {
@@ -54,7 +51,5 @@ public class WaveBlock extends WaveItem {
     public WaveBlock makeHoeEffective() { return this; }
 
     public ItemUseResult onUse(BlockState blockState, BlockPos pos, World world, EntityPlayer entityPlayer, UseHand useHand) {return null;}
-
-    public List<Text> addItemToolTip(ItemStack stack) {return Objects.requireNonNull(null);}
 
 }
