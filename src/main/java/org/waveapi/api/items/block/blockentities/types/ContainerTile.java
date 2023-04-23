a623bc96ee071df250b2220af437c539e91ba957
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-23 16:44:53 +0300

    Container fixes

--- src/main/java/org/waveapi/api/items/block/blockentities/types/ContainerTile.java
+++ src/main/java/org/waveapi/api/items/block/blockentities/types/ContainerTile.java
@@ -1,24 +1,32 @@
 package org.waveapi.api.items.block.blockentities.types;
 
-import org.waveapi.api.items.inventory.ItemStack;
+import org.waveapi.api.world.inventory.ItemStack;
 
 import java.util.Objects;
 
 public interface ContainerTile {
 
     int getSize();
-    ItemStack getStack(int slot);
-    void setStack(int slot, ItemStack stack);
+    org.waveapi.api.world.inventory.ItemStack getStack(int slot);
+    void setStack(int slot, org.waveapi.api.world.inventory.ItemStack stack);
 
-    default int giveItem(ItemStack stack) {
+    default int giveItem(org.waveapi.api.world.inventory.ItemStack stack) {
         return 0;
     }
 
-    default ItemStack take(int slot, int amount) {
+    default boolean isItemAllowedInSlot(int slot, ItemStack stack) {
+        return true;
+    }
+
+    default int giveItem(int slot, org.waveapi.api.world.inventory.ItemStack stack) {
+        return 0;
+    }
+
+    default org.waveapi.api.world.inventory.ItemStack take(int slot, int amount) {
         return Objects.requireNonNull(null);
     }
 
-    default ItemStack take(int slot) {
+    default org.waveapi.api.world.inventory.ItemStack take(int slot) {
         return Objects.requireNonNull(null);
     }
 }
