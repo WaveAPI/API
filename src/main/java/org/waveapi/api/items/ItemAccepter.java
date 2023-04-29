c2c61c6db80f2ffff2540f70376baf23bf59dd12
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-29 09:02:23 +0300

    Better ItemAccepter list

--- src/main/java/org/waveapi/api/items/ItemAccepter.java
+++ src/main/java/org/waveapi/api/items/ItemAccepter.java
@@ -6,7 +6,9 @@ public interface ItemAccepter {
     boolean canEnchant(WaveItem item);
 
     ItemAccepter ARMOUR = Objects.requireNonNull(null);
-    ItemAccepter WEAPON = Objects.requireNonNull(null);
+    ItemAccepter MElEE_WEAPON = Objects.requireNonNull(null);
+
+    ItemAccepter TOOL = Objects.requireNonNull(null);
     ItemAccepter MINING_TOOL = Objects.requireNonNull(null);
 
 }
